85e7b33d99729cf1690703d42226457bb85857fa
jubaer919 <60255197+jubaer919@users.noreply.github.com>
2023-05-25 20:20:45 +0600

    complete the testing part

--- src/operation.test.js
+++ src/operation.test.js
@@ -32,24 +32,24 @@ describe('Manipulate Add and delete Tasks', () => {
   });
 });
 
-// describe('Manipulate status and content updates', () => {
-//   document.body.innerHTML = '<div> <ul class="list"></ul> </div>';
+describe('Manipulate status and content updates', () => {
+  document.body.innerHTML = '<div> <ul class="list"></ul> </div>';
 
-//   const todo = new Todo();
+  const todo = new Todo();
 
-//   test('Clear all Completed', () => {
-//     todo.clearAll();
-//     expect(todo.tasks).toHaveLength(0);
-//   });
+  test('Clear all Completed', () => {
+    todo.clearAll();
+    expect(todo.tasks).toHaveLength(0);
+  });
 
-//   test('Editing task Description', () => {
-//     todo.addTask('Test 3');
-//     todo.editTask(0, 'Test 4');
-//     expect(todo.tasks[0]?.description).toBe('Test 4');
-//   });
+  test('Editing task Description', () => {
+    todo.addTask('Test 3');
+    todo.editTask(0, 'Test 4');
+    expect(todo.tasks[0]?.description).toBe('Test 4');
+  });
 
-//   test('Check task status', () => {
-//     todo.complete(0, true);
-//     expect(todo.tasks.at(0).completed).toBe(true);
-//   });
-// });
+  test('Check task status', () => {
+    todo.complete(0, true);
+    expect(todo.tasks.at(0).completed).toBe(true);
+  });
+});
